038e5a488930770940191aef3291c7d9b74b4090
DavidChierotti <DavidChierotti@LAPTOP-3C5AULFH>
2022-03-28 16:18:14 +0200

    versione senza controller

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,66 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	private Dictionary model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ChoiceBox<?> btnChoice;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnSpellCheck;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private TextArea txtUser;
+
+    @FXML
+    void doClearText(ActionEvent event) {
 
     }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+         String s=txtUser.getText();
+         
+    }
+    
+    
+    public void setModel(Dictionary model) {
+    	this.model=model;
+    }
+
+    @FXML
+    void initialize() {
+        assert btnChoice != null : "fx:id=\"btnChoice\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtUser != null : "fx:id=\"txtUser\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+
+
 }
-
-
